982834d47e2ea9d0e4b0815b6cd1ff6e81a6e307
jaburks79 <jaburks79@gmail.com>
2026-04-14 17:34:49 -0500

    Fix robots.txt serving from root URL

--- edgetravel/urls.py
+++ edgetravel/urls.py
@@ -4,10 +4,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from edgetravel.views import HomeView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     path('accounts/', include('accounts.urls')),
     path('destinations/', include('destinations.urls')),
     path('reports/', include('reports.urls')),
